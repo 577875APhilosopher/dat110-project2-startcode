fe1c8985ed09e2d05bf750d2524dc0f246d5f95d
Mati <586617@stud.hvl.no>
2021-02-24 13:23:55 +0100

    SubscribeMsg ferdig

--- src/no/hvl/dat110/messages/SubscribeMsg.java
+++ src/no/hvl/dat110/messages/SubscribeMsg.java
@@ -1,13 +1,28 @@
 package no.hvl.dat110.messages;
 
 public class SubscribeMsg extends Message {
-
-	// message sent from client to subscribe on a topic 
-
-	// TODO:
+	private String subscribeTo;
+	
 	// Implement object variables - a topic is required
+	
+	public SubscribeMsg(String user, String subscribeTo) {
+		super(MessageType.SUBSCRIBE, user);
+		this.subscribeTo = subscribeTo;
+	}
 
 	// Constructor, get/set-methods, and toString method
 	// as described in the project text
-		
+	
+	public String getSubscribeTo() {
+		return subscribeTo;
+	}
+	
+	public void setSubscribeTo(String subscribeTo) {
+		this.subscribeTo = subscribeTo;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " subscribe to: " + this.subscribeTo;
+	}
 }
